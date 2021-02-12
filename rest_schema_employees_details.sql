94d21754e6ed8e275ff5dbc5e1de40bbbc169c5a
Kaushik-cg-giri <78954349+Kaushik-cg-giri@users.noreply.github.com>
2021-02-12 15:41:07 +0530

    Add files via upload
    SQL command for the table creation

---
+++ rest_schema_employees_details.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE `employees_details` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `phone_number` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
+
+
+INSERT INTO `employees_details` VALUES (1,'Ajay','Edappally','945281010'),(2,'Binu','kurottu','8281454525'),(3,'Ebin','kanthn','757474747410'),(101,'Ajay','Edappally','945281010'),(102,'Binu','kurottu','8281454525'),(103,'Ebin','kanthn','757474747410');
+
+
